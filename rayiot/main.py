49f08008023e33eb85c4542def0bfe457db505cc
Oscar Orlando de la Paz Ibarra <125399948+delapazOscar@users.noreply.github.com>
2024-10-13 00:37:17 -0600

    [FIX]

--- rayiot/main.py
+++ rayiot/main.py
@@ -6,16 +6,16 @@ import requests_controller
 monitor = batteryTray.BatteryMonitor(addr=0x42)
 
 
-requests = requests_controller.RequestsController(
-    "http://18.116.231.95:8069/odoo-firebase-core/odoo-import",
-    "12fa06f23b81d89482ebadc754d20009272e2181e7c8f42759dbafcfd89c9c49",
-    1
+backend = requests_controller.RequestsController(
+    endpoint="http://18.116.231.95:8069/odoo-firebase-core/odoo-import",
+    access_token="12fa06f23b81d89482ebadc754d20009272e2181e7c8f42759dbafcfd89c9c49",
+    account_id=1
 )
 
 while True:
     monitor.run()
     battery_data = monitor.get_battery_data()
     print("Haciendo petición...")
-    requests.make_request("update_battery_data", battery_data, 1, "ray.rayiot")
+    backend.make_request("update_battery_data", battery_data, 1, "ray.rayiot")
     time.sleep(60)
 
