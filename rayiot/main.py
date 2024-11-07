b3b4dfab748df98d0544deb0b3be902bd375e125
Oscar Orlando de la Paz Ibarra <125399948+delapazOscar@users.noreply.github.com>
2024-11-07 00:40:19 -0600

    [ADD] try except petición

--- rayiot/main.py
+++ rayiot/main.py
@@ -16,6 +16,10 @@ while True:
     monitor.run()
     battery_data = monitor.get_battery_data()
     print("Haciendo petición...")
-    backend.make_request(method="update_battery_data", payload=battery_data, res_id=1, res_model="ray.rayiot")
+    try:
+        backend.make_request(method="update_battery_data", payload=battery_data, res_id=1, res_model="ray.rayiot")
+    except Exception as e:
+        print(f"Error en la petición: {e}")
+
     time.sleep(1)
 
