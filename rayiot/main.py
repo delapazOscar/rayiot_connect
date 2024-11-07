1196c8ca8f0b0a41f3440c517687ec6131b8b481
Oscar Orlando de la Paz Ibarra <125399948+delapazOscar@users.noreply.github.com>
2024-11-06 23:47:22 -0600

    [ADD] Cambio de endpoint

--- rayiot/main.py
+++ rayiot/main.py
@@ -7,7 +7,7 @@ monitor = batteryTray.BatteryMonitor(addr=0x42)
 
 
 backend = requests_controller.RequestsController(
-    endpoint="http://18.116.231.95:8069/odoo-firebase-core/odoo-import",
+    endpoint="http://rayiot.eastus2.cloudapp.azure.com:8070/odoo-firebase-core/odoo-import",
     access_token="12fa06f23b81d89482ebadc754d20009272e2181e7c8f42759dbafcfd89c9c49",
     account_id=1
 )
